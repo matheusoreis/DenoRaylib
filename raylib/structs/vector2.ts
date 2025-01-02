618cca847767531127595fc6bf37519078f4bb83
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2025-01-02 08:42:45 -0300

    Troca da fonte de consulta

--- raylib/structs/vector2.ts
+++ raylib/structs/vector2.ts
@@ -1,1 +1,22 @@
-export default class Vector2 {}
+export default class Vector2 {
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public x: number;
+  public y: number;
+
+  static fromBuffer(buffer: ArrayBuffer): Vector2 {
+    const view = new DataView(buffer);
+
+    const x = view.getFloat32(0, true);
+    const y = view.getFloat32(4, true);
+
+    return new Vector2(x, y);
+  }
+
+  get buffer(): ArrayBuffer {
+    return new Float32Array([this.x, this.y]).buffer;
+  }
+}
